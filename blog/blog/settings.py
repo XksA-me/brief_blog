975311daca7bc2176469452eaaeaba33c2ecfd50
xksa <pythonbrief@163.com>
2021-09-06 12:16:33 +0800

    mysql数据库配置

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',  # 数据库引擎
+        'NAME': 'brief_blog',  # 数据库名称
+        'USER': 'blog',  # 数据库用户名称
+        'PASSWORD': '123456',  # 数据库连接密码
+        'HOST': '127.0.0.1',  # 数据库主机地址
+        'PORT': '3306',  # 数据库连接端口
     }
 }
 
